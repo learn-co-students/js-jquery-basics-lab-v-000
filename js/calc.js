0b3f975e1bb80659f33176b85d3756084c9687cf
pacosta613 <pedro.acosta613@gmail.com>
2016-04-07 10:35:44 -0400

    Done.

--- js/calc.js
+++ js/calc.js
@@ -1,40 +1,30 @@
-'use strick';
-
 $(document).ready(function(){
   doMath();
 });
 
-function doMath() {
-  $('#equals').on('click', function() {
+function doMath(){
+  $('#equals').on("click", function(){
     var number1 = parseInt($('#number1').val());
-    var number2 = parseInt($('#number2').val());
     var operation = $('#operation').val();
-  });
+    var number2 = parseInt($('#number2').val());
+    validate(operation, number1, number2);
+  })
 }
 
-function validate(operator, number1, number2) {
-  if (isNaN(number1) || isNaN(number2)) {
+function validate(operator, number1, number2){
+  if((isNaN(number1)) || (isNaN(number2))){
     return "Sorry, one of those is not a valid number!";
-  } 
-  else if (operator == "+" && operator == "-" && operator == "*" && operator == "/") {
-    return result(number1, operator, number2);
-  } 
-  else {
+  }
+  else if (operator != "+" && operator != "-" && operator != "*" && operator != "/"){
     return "Sorry, that is not a valid operator";
   }
+  else { 
+    result(operator, number1, number2);
+  }
 }
 
-function result(operator, number1, number2) {
-  if (operator == "*") {
-    return number1 * number2;
-  }
-  else if (operator == "-") {
-    return number1 - number2;
-  }
-  else if (operator == "/") {
-    return number1 / number2;
-  }
-  else if (operator == "+") {
-    return number1 + number2;
-  }
-}
+function result(operator, number1, number2){
+  var total = eval(number1 + operator + number2);
+    $('#result').text(total);
+    return total;
+  } 
