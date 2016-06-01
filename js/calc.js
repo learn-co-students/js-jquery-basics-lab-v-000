1b70ac8859af47a941933da06069033c6aaf474c
plai217 <philiplai217@gmail.com>
2016-06-01 00:20:24 -0400

    Done.

---
+++ js/calc.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+  doMath();
+
+});
+
+var num1 = $('#number1');
+var num2 = $('#number2');
+var operator = $('#operation');
+
+
+function doMath(){
+  $('#equals').on('click', function(){
+    num1 = parseFloat(num1.val());
+    num2 = parseFloat(num2.val());
+    operator = operator.val();
+    validate(operator, num1, num2);
+  });
+}  
+
+function validate(operator, num1, num2) {
+  if(operator !== '*' && operator !== '+' && operator !== '/' && operator !== '-') {
+    return "Sorry, that is not a valid operator";
+  } else if(isNaN(num1) || isNaN(num2)) {
+    return "Sorry, one of those is not a valid number!";
+  } else {
+    result(operator, num1, num2);
+  }
+}
+
+function result(operator, num1, num2) {
+  var answer = 0;
+  if(operator == "+") {
+    answer = num1 + num2;
+  } else if(operator == "-") {
+    answer = num1 - num2;
+  } else if(operator == "/") {
+    answer = num1 / num2;
+  } else {
+    answer = num1 * num2;
+  }
+  $('#result').html(answer);
+  return answer;
+}
