51b936c14613ce0b0fbd8a5bcc11458e9fe74d80
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-15 15:46:14 -0400

    Done.

--- js/calc.js
+++ js/calc.js
@@ -9,7 +9,7 @@ function doMath() {
   $('#equals').on("click", function() {
     var num1 = $('#number1').val() , num2 = $('#number2').val(), operator = $('#operation').val();
     
-    if (validate(num1, num2, operator) == true) {
+    if (validate(operator, num1, num2) == true) {
        $("#result").text(function(){
          result(operator, firstNum, secondNum);
        });
@@ -22,16 +22,15 @@ function doMath() {
 function validate(operator, num1, num2) {
   // use isNaN
   if (!operator in operators) {
-    return "Sorry, that is not a valid operator.";
+    return "Sorry, that is not a valid operator";
   } else if(isNaN(num1) || isNaN(num2)) {
     return "Sorry, one of those is not a valid number!";
   } else {
-    return true;
+    return "Sorry, that is not a valid operator";
   }
 }
 
 function result(operator, num1, num2) {
-  // why is operators[operator] not a function because the validate is not returning what expected
   return operators[operator](num1, num2);
 }
 
