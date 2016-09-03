6a44abe593e37c3cce905e8a70d30607626aab10
shoko furushita <shktkt219@gmail.com>
2016-09-03 12:23:22 -0400

    Done.

---
+++ js/calc.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  doMath();
+})
+
+var num1 = $('#number1'),
+    num2 = $('#number2'),
+    operator = $('#operation');
+
+
+function doMath(){
+  $('#equals').on('click', function(){
+    num1 = parseFloat(num1, val());
+    num2 = parseFloat(num2, val());
+    operator = operator.val();
+    validate(operator, num1, num2);
+  });
+}
+
+function validate(operator, num1, num2){
+  if (operator == "+" || operator == "-" || operator == "*" || operator == "/"){
+    if (isNaN(num1) || num1 === '' || isNaN(num2) || num2 === ''){
+       $('#result').text('Sorry, one of those is not a valid number!');
+       return 'Sorry, one of those is not a valid number!'
+      }
+      else {
+        result(operator, num1, num2);
+      }
+    }
+    else {
+      $('#result').text('Sorry, that is not a valid operator');
+      return 'Sorry, that is not a valid operator'
+    }
+  }
+
+function result(operator, num1, num2){
+  if (operator == '+'){
+    $('#result').text(num1 + num2);
+    return num1 + num2;
+  }
+  else if (operator == '-'){
+    $('#result').text(num1 - num2);
+    return num1 - num2;
+  }
+  else if (operator == '*'){
+    $('#result').text(num1 * num2);
+    return num1 * num2;
+  }
+  else if (operator == '/'){
+    $('#result').text(num1 / num2);
+    return num1 / num2;
+  }
+}
