147da90e5140bcda76b2bc96fe4190222ca85bbc
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-26 10:30:29 -0400

    working in the browser now too

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  doMath;
+  doMath();
 });
 
 var num_1, num_2, operation;
@@ -9,15 +9,15 @@ function doMath() {
   num_1 = Number($('input#number1').val());
   num_2 = Number($('input#number2').val());
   operation = $('input#operation').val();
-  });
   validate(operation, num_1, num_2);
+  });
 }
 
 function validate(op, num_1, num_2) {
   if (num_2.constructor.name != "Number" || num_1.constructor.name != "Number") {
     alert("Sorry, one of those is not a valid number!");
     return "Sorry, one of those is not a valid number!";
-  } else if (op != "/" || op != "-" || op != "+" || op != "*") {
+  } else if (op != "/" && op != "-" && op != "+" && op != "*") {
     alert("Sorry, that is not a valid operator");
     return "Sorry, that is not a valid operator";
   } else {
