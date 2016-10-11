3098a2f2f0508523b8884f7e54f0eeea847d0e95
kaileeagray <kaileeagray@gmail.com>
2016-10-11 17:44:54 -0500

    Done.

--- js/calc.js
+++ js/calc.js
@@ -2,36 +2,35 @@ $(document).ready(function(){
   doMath();
 });
 
-var number1 = $('#number1');
-var number2 = $('#number2');
-var operation = $('#operation');
+
 
 function doMath() {
   $('button').on('click', function() {
-    number1 = parseInt(number1);
-    number2 = parseInt(number2);
-    validate(number1, number2, operation);
+    var number1 = $('#number1').val();
+    var number2 = $('#number2').val();
+    var operation = $('#operation').val();
+    validate(operation, number1, number2);
   });
 }
 
-function result(num1, num2, operation) {
-  var sol = eval([num1, operation, num2].join(" "));
+function result(operation, number1, number2) {
+  var sol = eval([number1, operation, number2].join(" "));
   $('#result').replaceWith("<h2 id='result'>" + sol + "</h2>");
   return sol;
 }
 
-function validate(number1, number2, operation) {
-  var isNum = !isNaN(parseFloat(number1)) && !isNaN(parseFloat(number2));
-  if (!isNum) {
+function validate(operation, number1, number2) {
+  var isNotNum = isNaN(number1) || isNaN(number2);
+  var isOp = ["+", "-", "*", "/"].indexOf(operation) > -1;
+  if (isNotNum) {
     $("#result").text('Sorry, one of those is not a valid number!');
     return "Sorry, one of those is not a valid number!";
   }
-  var isOp = ["+", "-", "*", "/"].indexOf(operation) > -1;
   if (!isOp) {
     $("#result").text('Sorry, that is not a valid operator');
     return "Sorry, that is not a valid operator";
   }
-  if (isNum && isOp) {
-    result(number1, number2, operation);
+  if (!isNotNum && isOp) {
+    result(operation, number1, number2);
   }
 }
