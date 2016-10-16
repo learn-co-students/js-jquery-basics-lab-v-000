1217dff5e1f7fbce1fcd79f23e45a21ba5b47510
KerryAlsace <kerrydougherty@gmail.com>
2016-10-16 16:05:37 -0400

    Done.

--- js/calc.js
+++ js/calc.js
@@ -1,31 +1,39 @@
+$(document).ready(function() {
+  doMath();
+});
+
+
 function doMath() {
-  // save variables
-  var num1 = $('#number1').val();
-  num1 = parseInt(num1)
-  var operator = $('#operation').val();
-  var num2 = $('#number2').val();
-  num2 = parseInt(num2)
-  // validate
-  $('button#equals').on('click', validate(num1, operator, num2) {
-    return validate(num1, operator, num2);
+  $('#equals').on('click', function() {
+    var num1 = $('#number1').val();
+    num1 = parseInt(num1)
+    var operator = $('#operation').val();
+    var num2 = $('#number2').val();
+    num2 = parseInt(num2)
+    validate(operator, num1, num2)
   });
 }
 
-function validate(num1, operator, num2) {
-  var validOperators = ["*", "/", "+", "-"]
+function validate(operator, num1, num2) {
+  var validOperators = ["*", "/", "+", "-"];
   // if num1 and num2 are not type integer
   if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    console.log($('#result').text("Sorry, one of those is not a valid number!"))
     return "Sorry, one of those is not a valid number!"
-  };
+  }
   // if operator is not one of the validOperators
-  if ( !(validOperators.indexOf(operator) >= 0) ){
+  else if ( !(validOperators.indexOf(operator) >= 0) ){
+    console.log($('#result').text("Sorry, that is not a valid operator"))
     return "Sorry, that is not a valid operator"
+  }
+  else {
+    result(operator, num1, num2);
   };
-  return result(num1, operator, num2)
 }
 
-function result(num1, operator, num2) {
-  var array = [num1, operator, num2]
-  var newArray = array.join(" ")
-  $('div#result').append(eval(newArray))
+function result(operator, num1, num2) {
+  var array = [num1, operator, num2];
+  var newArray = array.join(" ");
+  console.log(($('#result').html(eval(newArray))));
+  return eval(newArray);
 }
