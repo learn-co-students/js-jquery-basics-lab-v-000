8b249dcdbcc2b246179a58e1e3c64d92bde66538
Lani1321 <lani.berry13@gmail.com>
2016-11-11 17:23:01 -0500

    Done.

--- js/calc.js
+++ js/calc.js
@@ -13,8 +13,9 @@ function doMath() {
   })
 }
 
+
 function validate(operator, num1, num2) {
-  if (operator == " + " || operator == " - " || operator == " * " || operator == " / "){
+  if (operator == "+" || operator == "-" || operator == "*" || operator == "/"){
     if (isNaN(num1) || num1 === '' || isNaN(num2) || num2 === '' ){
       $("#result").text('Sorry, one of those is not a valid number!');
       return 'Sorry, one of those is not a valid number!'
