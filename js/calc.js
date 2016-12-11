a0ccbf48f969f02bb6bbdabf3bfe7bb1b79a953a
Yechiel K <chilikkal@gmail.com>
2016-12-10 23:34:43 -0500

    Done.

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,7 @@
+function result(operator, num1, num2) {
+  return eval(num1 + operator + num2);
+}
+
 function validate(operator, num1, num2) {
   var num1 = num1;
   var operator = operator;
@@ -8,10 +12,20 @@ function validate(operator, num1, num2) {
     return "Sorry, that is not a valid operator";
   } else if (isNaN(num1) || isNaN(num2)) {
    return "Sorry, one of those is not a valid number!";
-  }
+ } else {
+   return result(operator, num1, num2)
+ }
 }
 
-
+function doMath() {
+  $('#equals').on('click', function() {
+    var operator = $('#operation').val();
+    var num1 = $('#number1').val();
+    var num2 = $('#number2').val();
+    var result = validate(operator, num1, num2)
+    $('#result').text(result);
+  });
+}
 
 $(document).ready(function() {
   doMath()
