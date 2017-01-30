5432c8b09dbaf5154a0f52feb63a01fe34a4712a
Joe <jsbenning4@gmail.com>
2017-01-30 09:14:23 -0500

    Done.

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,12 @@
-var num1 = NaN;
-var num2 = NaN;
-var op = null;
+//var num1 = NaN;
+//var num2 = NaN;
+//var op = null;
 
 function doMath() {
-  $("button#equals").click(function() {
-    num1 = $("input#number1").val();
-    num2 = $("input#number2").val();
-    op = $("input#operation").val();
+  $("#equals").on("click", function() {
+    num1 = parseFloat($("#number1").val());
+    num2 = parseFloat($("#number2").val());
+    op = $("#operation").val();
     validate(op, num1, num2);
   });
 }
@@ -14,33 +14,33 @@ function doMath() {
 
 function validate(op, num1, num2) {
   var operators = ["+", "-", "*", "/"];
+
   if (!(operators.includes(op))) {
-    $("#result").html("Sorry, that is not a valid operator");
+    $("#result").text("Sorry, that is not a valid operator");
     return "Sorry, that is not a valid operator";
 
-  } else if ((num1 == "") || (num2 == "")) {
-    $("#result").html("Sorry, one of those is not a valid number!");
-    return "Sorry, one of those is not a valid number!";
-
-  } else if (isNaN(Number(num1)) || isNaN(Number(num2))) { 
-    $("#result").html("Sorry, one of those is not a valid number!");
+  } else if ( (num1 == "") || (num2 == "") || (isNaN(num1)) || (isNaN(num2)) ) {
+    $("#result").text("Sorry, one of those is not a valid number!");
     return "Sorry, one of those is not a valid number!";
 
   } else {
-    val = result(op, num1, num2);
-    return val;
+    result(op, num1, num2);
   }
 }
 
 
 function result(op, num1, num2) {
   ans = (eval(num1 + op + num2));
-  $("#result").html(ans);
+  $("#result").text(ans);
   return ans;
 }
 
 
 $(document).ready(function() {
+  var num1 = NaN;
+  var num2 = NaN;
+  var op = null;
   doMath();
 
 });
+
