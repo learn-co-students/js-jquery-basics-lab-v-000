bc5b04147d4e7ff24e6d8c508d8386994cfe3dbe
lizdenhup <lizdenhup@gmail.com>
2017-02-10 15:29:36 -0600

    3 methods working

--- js/calc.js
+++ js/calc.js
@@ -1,17 +1,27 @@
+$(document).ready(function() {
+  doMath();
+});
+
+var num1 = $('number1');
+var num2 = $('number2');
+var operator = $('#operation');
+
 function doMath() {
   $("#equals").on("click", function() {
-    number1 = parseInt($("#number1").val(), 10);
-    number2 = parseInt($("#number2").val(), 10);
-    operator = $("operation").val();
-    validate(operator, number1, number2);
-  })
+    num1 = parseInt($("#number1").val(), 10);
+    num2 = parseInt($("#number2").val(), 10);
+    operator = operator.val();
+    validate(operator, num1, num2);
+  });
 }
 
-function validate(operator, number1, number2) {
-  if () {
-
-  } else if () {
-
+function validate(operator, num1, num2) {
+  if (isNaN(num1) || num1 === "" || isNaN(num2) || num2 === "") {
+    $("#result").text("Sorry, one of those is not a valid number!");
+    return 'Sorry, one of those is not a valid number!'
+  } else if (/.[\+\-\*\/]/.test(operator) === false) {
+    $("#result").text("Sorry, that is not a valid operator");
+    return 'Sorry, that is not a valid operator'
   } else {
     result(operator, number1, number2);
   }
@@ -21,6 +31,3 @@ function result(operator, number1, number2) {
 
 }
 
-$(document).ready(function() {
-  doMath();
-});
